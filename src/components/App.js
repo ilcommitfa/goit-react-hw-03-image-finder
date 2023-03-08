97ea1a79ace06438a579a40cb05fda6fc03bf985
ilcommitfa <110323544+ilcommitfa@users.noreply.github.com>
2023-03-08 18:57:41 +0200

    update

--- src/components/App.js
+++ src/components/App.js
@@ -1,45 +1,107 @@
-import { useState } from 'react';
-import { nanoid } from 'nanoid';
-import ContactForm from './ContactForm';
-import ContactList from './ContactList';
-import Filter from './Filter';
-import {Container} from './App.styled';
+import React, { Component } from 'react';
+import Searchbar from './Searchbar';
+import ImageGallery from './ImageGallery';
+import Loader from './Loader';
+import Button from './Button';
+import Modal from './Modal';
+import './styles.css';
 
-function App() {
-  const [contacts, setContacts] = useState([]);
-  const [filter, setFilter] = useState('');
+class App extends Component {
+  state = {
+    images: [],
+    loading: false,
+    error: null,
+    query: '',
+    currentPage: 1,
+    showModal: false,
+    modalImage: '',
+    modalAlt: ''
+  };
 
-  const addContact = (name, number) => {
-    const contact = { id: nanoid(), name, number };
-    if (contacts.find((item) => item.name.toLowerCase() === name.toLowerCase())) {
-      alert(`${name} is already in contacts`);
-      return;
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.query !== this.state.query) {
+      this.fetchImages();
     }
-    setContacts([contact, ...contacts]);
+  }
+
+  handleSearchbarSubmit = query => {
+    this.setState({ query: query, currentPage: 1, images: [] });
   };
 
-  const deleteContact = (id) => {
-    setContacts(contacts.filter((item) => item.id !== id));
+  handleLoadMore = () => {
+    this.setState(prevState => ({
+      currentPage: prevState.currentPage + 1,
+      isLoading: true,
+    }));
+  
+    this.fetchImages();
+  };
+  
+  handleImageClick = (largeImageURL, tags) => {
+    this.setState({
+      showModal: true,
+      modalImage: largeImageURL,
+      modalAlt: tags
+    });
   };
 
-  const changeFilter = (e) => {
-    setFilter(e.target.value);
+  handleCloseModal = () => {
+    this.setState({
+      showModal: false,
+      modalImage: '',
+      modalAlt: ''
+    });
   };
 
-  const filteredContacts = contacts.filter((item) =>
-    item.name.toLowerCase().includes(filter.toLowerCase())
-  );
+  fetchImages = () => {
+    const { query, currentPage } = this.state;
+    const apiKey = '34227355-634b3cfb76d00133b4cb8e037';
+    const url = `https://pixabay.com/api/?key=${apiKey}&q=${query}&page=${currentPage}&per_page=12`;
 
-  return (
-    <Container>
-      <h1>Phonebook</h1>
-      <ContactForm onAddContact={addContact} />
+    this.setState({ loading: true });
 
-      <h2>Contacts</h2>
-      <Filter value={filter} onChange={changeFilter} />
-      <ContactList contacts={filteredContacts} onDeleteContact={deleteContact} />
-    </Container>
-  );
+    fetch(url)
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error('Something went wrong with network request');
+      })
+      .then(data => {
+        this.setState(prevState => ({
+          images: [...prevState.images, ...data.hits],
+          loading: false
+        }));
+        window.scrollTo({
+          top: document.documentElement.scrollHeight,
+          behavior: 'smooth'
+        });
+      })
+      .catch(error => this.setState({ error, loading: false }));
+  };
+
+  render() {
+    const { images, loading, error, showModal, modalImage, modalAlt } = this.state;
+
+    return (
+      <div className="App">
+        <Searchbar onSubmit={this.handleSearchbarSubmit} />
+        {error && <p>Error: {error.message}</p>}
+        <ImageGallery images={images} onImageClick={this.handleImageClick} />
+        {loading && <Loader />}
+        {images.length > 0 && !loading && (
+          <Button onLoadMore={this.handleLoadMore} />
+        )}
+        {showModal && (
+          <Modal
+            onClose={this.handleCloseModal}
+            image={modalImage}
+            alt={modalAlt}
+          />
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Filter.styled.js
+++
@@ -1,8 +0,0 @@
-import styled from "@emotion/styled";
-
-export const Label = styled.label`
-  display: flex;
-  gap: 4px;
-  margin-bottom: 12px;
-  align-items: center;
-  `;
