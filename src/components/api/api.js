9fc992e95a10316c924adba7a15e431002941804
ilcommitfa <110323544+ilcommitfa@users.noreply.github.com>
2023-03-24 20:07:28 +0200

    update

--- src/components/api/api.js
+++ src/components/api/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = '34227355-634b3cfb76d00133b4cb8e037';
 
-const fetchImages = async (query, page = 1) => {
+const fetchImages = async (query, page) => {
   const url = `${BASE_URL}?q=${query}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`;
   const response = await axios.get(url);
 
